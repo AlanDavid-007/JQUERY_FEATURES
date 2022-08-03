5211c9f7765ad07cb0c0af9a5eb85d253b4a78eb
Alan <alanpereiradavid@gmail.com>
2022-08-02 21:34:55 -0300

    Fades

--- Assets/Js/index.js
+++ Assets/Js/index.js
@@ -1,8 +1,34 @@
 // A $( document ).ready() block.
-$( document ).ready(function() {
-
-    $( ".button-teste" ).click(function() {
-        $( ".teste" ).hide();
-        
-      });
+$(document).ready(function () {
+  $(".button-teste-numero").click(function () {
+    $(".button-teste-numero").css('color', 'white');
+    $(".teste").css('color', 'white');
+    // $(this).text('Processing…');
+    var valor = $(this).val();
+    $('.teste').text(valor);
+    //   $( ".button-teste-numero" ).click(function() {   
+    //     $( '.teste' ).css('color', 'red');    
+    //     $('.teste').text('Urubu');        
+    // });                  
+  });
+  $('.button-teste-texto').click(function() {
+    var nome = $(this).attr('name');
+    $('.teste').text(nome);
+  })
+  $(".button-teste-fadein").click(function () {
+    $('.teste').fadeIn();
+  });
+  $(".button-teste-fadeout").click(function () {
+    $('.teste').fadeOut();
+  });
+  $(".button-fade1").click(function () {
+    $('.img1').fadeToggle();
+  });
+  $(".button-fade2").click(function () {
+    $('.img2').fadeToggle();
+  });
+  $(".button-fade3").click(function () {
+    $('.img3').fadeToggle();
+  });
 });
+
--- index.php
+++ index.php
@@ -8,14 +8,77 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="./Assets/Css/index.css?date=<?php echo time()?>">
+    <link rel="stylesheet" href="./Assets/Css/index.css?date=<?php echo time() ?>">
     <title>JQUERY EXERCISES</title>
 </head>
 
 <body>
-    <h1 class="teste">Hello, world!</h1>
-    <button class="btn btn-primary button-teste">Aperta aqui</button>
-
+    <!-- <h1 class=" teste text-light">olaaaaaaaaaaaaaaaa</h1>
+    <button class="btn btn-primary button-teste" value="1">Hello, world!</button> -->
+    <!-- <div class="moon">
+        <div class="circle"></div>
+    </div> -->
+    <div class="container d-flex justify-content-center align-tems-center mt-5 pt-5">
+        <div class="login-box mt-5 mb-5 pt-5">
+            <h2 class="teste">Login</h2>
+            <form>
+                <div class="user-box">
+                    <input type="text" name="" required="">
+                    <label>Username</label>
+                </div>
+                <div class="user-box">
+                    <input type="password" name="" required="">
+                    <label>Password</label>
+                </div>
+                <!-- <a href="#">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    Click me!
+                    </a> -->
+                    <div class="container d-flex justify-content-around buttons mb-3">
+                <button class="btn btn-primary button-teste-numero" value="1">Numero</button>
+                <button class="btn btn-primary button-teste-texto" name="Funcionou">Texto</button>
+                </div>
+                <div class="container d-flex justify-content-around buttons">
+                <button class="btn btn-primary button-teste-fadein" value="1">Fade in</button>
+                <button class="btn btn-primary button-teste-fadeout" value="1">Fade out</button>
+                </div>
+            </form>
+        </div>
+    </div>
+    <div class="container d-flex justify-content-around">
+        <div class="card img1" style="width: 18rem;">
+            <img class="card-img-top" src="https://cdn.pixabay.com/photo/2022/07/25/15/18/cat-7344042_960_720.jpg" alt="Card image cap">
+        </div>
+        <div class="card img2" style="width: 18rem;">
+            <img class="card-img-top" src="https://cdn.pixabay.com/photo/2020/04/30/03/26/rufous-5111261_960_720.jpg" alt="Card image cap">
+        </div>
+        <div class="card img3" style="width: 18rem;">
+            <img class="card-img-top" src="https://cdn.pixabay.com/photo/2022/07/26/13/55/egg-7345934_960_720.jpg" alt="Card image cap">
+        </div>
+    </div>
+    <div class="container d-flex justify-content-around mt-3">
+        <button class="btn btn-primary button-fade1" value="1">Hello, world!</button>
+        <button class="btn btn-primary button-fade2" value="1">Hello, world!</button>
+        <button class="btn btn-primary button-fade3" value="1">Hello, world!</button>
+    </div>
+    <!-- <div class="container d-flex justify-content-center mt-3 card-mobile">
+        <div class="card img1" style="width: 18rem;">
+            <img class="card-img-top" src="https://cdn.pixabay.com/photo/2022/07/25/15/18/cat-7344042_960_720.jpg" alt="Card image cap">
+        </div>
+    </div>
+    <div class="container d-flex justify-content-center mt-3 card-mobile">
+        <div class="card img2" style="width: 18rem;">
+            <img class="card-img-top" src="https://cdn.pixabay.com/photo/2020/04/30/03/26/rufous-5111261_960_720.jpg" alt="Card image cap">
+        </div>
+    </div>
+    <div class="container d-flex justify-content-center mt-3 card-mobile">
+        <div class="card img3" style="width: 18rem;">
+            <img class="card-img-top" src="https://cdn.pixabay.com/photo/2022/07/26/13/55/egg-7345934_960_720.jpg" alt="Card image cap">
+        </div>
+    </div> -->
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
